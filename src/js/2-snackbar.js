ab45f3db30983360e4f3145dcb040b03e887093b
OlenaDmytriieva <lela.dmitrieva@gmail.com>
2024-02-12 17:43:55 +0200

    2-snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,40 @@
+'use strict';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const input = document.querySelector('input[name="delay"]');
+// const radioBtn = document.querySelector('fieldset');
+const submitBtn = document.querySelector('button');
+
+submitBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+  const radioBtn = document.querySelector('input[name="state"]:checked');
+  const delay = input.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (radioBtn.value === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        icon: '',
+        messageSize: '16px',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        icon: '',
+        messageSize: '16px',
+      });
+    });
+});
